fdf8d97836654dc0b23b70832659a679cec64a70
Abdullah Akram Ansari <abdullaha@nulogy.com>
2020-04-24 15:49:59 -0400

    F/E WIP

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 const MAP = L.map('map_id').setView([38, -95], 4);
 L.tileLayer.provider('CartoDB.Positron').addTo(MAP);
+
+var info = L.control();
+
+info.onAdd = function (MAP) {
+  this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+  this.update();
+  return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+  this._div.innerHTML = '<h4>Texas</h4>' +  (props ?
+    '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+    : 'Hover over a state');
+};
+
+info.addTo(MAP);
